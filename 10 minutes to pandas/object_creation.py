40f85478df1133fd1a64af57ad3d9cc665750b6a
harilal766 <harilalsunil2@gmail.com>
2025-07-03 14:54:20 +0530

    10 min : Creating a DataFrame by passing a dictionary of objects

--- 10 minutes to pandas/object_creation.py
+++ 10 minutes to pandas/object_creation.py
@@ -2,22 +2,19 @@ import numpy as np
 import pandas as pd
 
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
 
-#print(s)
+# Object creation
+s = pd.Series([1,3,5,np.nan,6,8])
 
 dates = pd.date_range("20130101", periods=6)
 
-#print(dates)
-
-
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list("ABCD"))
-
+# df
+df = pd.DataFrame(np.random.randn(6,4), index=dates,columns=list("ABCD"))
 
 df2 = pd.DataFrame(
     {
-        "A": 1.0,
-        "B": pd.Timestamp("20130102"),
+        "A" : 1.0,
+        "B" : pd.Timestamp("20130102"),
         "C": pd.Series(1, index=list(range(4)), dtype="float32"),
         "D": np.array([3] * 4, dtype="int32"),
         "E": pd.Categorical(["test", "train", "test", "train"]),
@@ -25,5 +22,4 @@ df2 = pd.DataFrame(
     }
 )
 
-
-print(df2.to_numpy())
+print(df2.dtypes)
---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+
+[10 min](https://pandas.pydata.org/docs/user_guide/10min.html#min)
