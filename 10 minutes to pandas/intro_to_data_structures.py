9de70aadf59d3981227208ee49110d2c0223459f
harilal766 <harilalsunil2@gmail.com>
2025-07-03 17:10:59 +0530

    Intro to data structures : Series is ndarray-like

--- 10 minutes to pandas/intro_to_data_structures.py
+++ 10 minutes to pandas/intro_to_data_structures.py
@@ -5,19 +5,27 @@ import pandas as pd
 https://pandas.pydata.org/docs/user_guide/dsintro.html#dsintro
 """
 # Series
-
 limit = 5
 data = list(range(1, limit+1))
 index = ["a", "b","c","d","e"]
 
+# From ndarray
 s = pd.Series(data,index=index)
-
 ser = pd.Series(np.random.randn(limit))
 
 # From dict
-
 d = {"b": 1, "a": 0, "c": 2}
 dfd = pd.Series(d)
 dfd = pd.Series(d, index=["b", "c", "d", "a"]) # adding a new index will show result as NaN, a std data marker for missing data
 
-print(dfd)
+# From scalar value
+scalar = pd.Series(5.0, index=["a", "b", "c", "d", "e"])
+
+
+print(s)
+#print(s.iloc[0])
+#print(s == 4)
+#print(s[s == 4])
+#print(s.iloc[[4, 3, 2, 1]])
+#print(s.iloc[[4, 3, 2, 1]])
+print(np.exp(s))
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 
-
-
+[intro into data structures](https://pandas.pydata.org/docs/user_guide/dsintro.html#dsintro)
 [10 min](https://pandas.pydata.org/docs/user_guide/10min.html#min)
